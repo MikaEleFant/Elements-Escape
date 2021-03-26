c81080584f8af1255e35a9b0b59d15f3baa2c98f
MikaEleFant <53496914+MikaEleFant@users.noreply.github.com>
2021-03-26 16:14:04 -0700

    Add Room base class, extend Room1 class

--- levels/level1.js
+++ levels/level1.js
@@ -1,5 +1,25 @@
 import Room from "room.js";
 
 class Room1 extends Room {
-  
+  constructor(items) {
+    super(items);
+  }
+
+  use(obj1, obj2) {
+    super(use(obj1, obj2));
+  }
+
+  inspect(obj) {
+    super(inspect(obj));
+  }
+
+  pickup(obj) {
+    super(pickup(obj));
+  }
+
+  combine(obj1, obj2) {
+    super(combine(obj1, obj2));
+  }
 }
+
+export default Room1;
