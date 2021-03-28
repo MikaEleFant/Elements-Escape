83561a658d120373728ab0eec5af60df32f17a37
MikaEleFant <53496914+MikaEleFant@users.noreply.github.com>
2021-03-28 12:13:46 -0700

    Create fetch promise for all JSON levels

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-// // import Game from "./src/game.js";
-// const levelURLs = ["./src/levels/level1.json"];
-// let levels;
+// import Game from "./src/game.js";
+const levelURLs = ["./src/levels/level1.json"];
+let levels;
 
-// function getLevel(levelURL) {
-//   return fetch(levelURL)
-//     .then(response => {
-//       return response.json();
-//     })
-//     .then(level => {
-//       return Promise.resolve(level);
-//     })
-// }
+function getLevel(levelURL) {
+  return fetch(levelURL)
+    .then(response => {
+      return response.json();
+    })
+    .then(level => {
+      return Promise.resolve(level);
+    })
+}
 
-// Promise.all(
-//   levelURLs.map(getLevel)
-// ).then((data) => {
-//   levels = data;
-//   console.log(levels);
-// })
+Promise.all(
+  levelURLs.map(getLevel)
+).then((data) => {
+  levels = data;
+  console.log(levels);
+})
