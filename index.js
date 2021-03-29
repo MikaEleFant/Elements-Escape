0ff0126ba2018301da05c00904dfa2fb0a9c48de
MikaEleFant <53496914+MikaEleFant@users.noreply.github.com>
2021-03-28 19:05:32 -0700

    Style game screen, implement logs append

--- index.js
+++ index.js
@@ -7,6 +7,24 @@ levelItems.push(level1Items);
 console.log(levelItems[0]);
 let game = new Game(levelStartText, levelItems);
 
-function startGame() {
+let entry = document.querySelector("input.entry");
+let entryButton = document.querySelector("button.entry-button");
+
+entry.addEventListener("keyup", function(event) {
+  event.preventDefault();
+  if (event.keyCode === 13) {
+    entryButton.click();
+  }
+})
+
+entry.addEventListener("click", function(event) {
+  event.preventDefault();
+  if (event.detail === 3) {
+    entryButton.click();
+  }
+})
+
+entryButton.addEventListener("click", function(event) {
+  event.preventDefault();
   game.start();
-}
+})
